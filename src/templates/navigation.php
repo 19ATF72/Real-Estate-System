1087f3dc3caeede634a9b9b37984634c4a90d3ef
Andrew De Torres <andrew.de.torres@hotmail.co.uk>
2019-08-28 13:09:26 +0000

    Continue styling of navigation, moved login and signup out of templates

--- src/templates/navigation.php
+++ src/templates/navigation.php
@@ -1,40 +1,38 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-  <a class="navbar-brand text-light ml-2" href="#">MOVE</a>
+  <a class="navbar-brand text-light ml-2 logo-font" href="#">MOVE</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  
+
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
+    </ul>
+    <ul class="navbar-nav navbar-right mr-2">
+      <li class="nav-item  my-auto mx-3">
         <a class="nav-link text-light" href="#">Home</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="#">Link 1</a>
+      <li class="nav-item my-auto mx-3">
+        <a class="nav-link text-light" href="#">Buy</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="#">Link 2</a>
+      <li class="nav-item my-auto mx-3">
+        <a class="nav-link text-light" href="#">Rent</a>
+      </li>
+      <li class="nav-item my-auto mx-3">
+        <a class="nav-link text-light" href="#">Commercial</a>
+      </li>
+      <li class="nav-item my-auto mr-2">
+        <div class="vertical-divider"><div>
+      </li>
+      <li class="nav-item my-auto">
+        <a class="nav-link" href="#">
+          <button class="btn btn-outline-light" type="submit">Log In</button>
+        </a>
+      </li>
+      <li class="nav-item my-auto">
+        <a class="nav-link" href="#">
+          <button class="btn btn-outline-light" type="submit">Sign Up</button>
+        </a>
       </li>
     </ul>
-    <ul class="nav navbar-nav navbar-right">
-
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <button class="btn btn-outline-light" type="submit">Log Out</button>
-          </a>
-        </li>
-        <div class="vertical-devider"><div>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" alt="Avatar" class="avatar rounded-circle mr-2"> 
-          </a>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-      </ul>
     </div>
   </nav>
