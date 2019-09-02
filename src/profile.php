74458eb20dfdc882a4496e898ed8339c4f1bb2a0
Andrew De Torres <andrew.de.torres@hotmail.co.uk>
2019-09-02 15:10:41 +0000

    Add table to profile

--- src/profile.php
+++ src/profile.php
@@ -38,42 +38,99 @@ $navIsLight = false;
       <div class="row">
         <div class="col-lg-8 col-sm-12">
           <div class="row">
-              <div class="col-6 mb-4">
-                <a href="/my-properties.php" class="text-decoration-none">
-                  <div class="card border-left-custom border-custom shadow h-100 py-2">
-                    <div class="card-body">
-                      <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                          <h6 class="font-weight-bold text-dark text-uppercase mb-1">Properties Listed</h6>
-                          <p class="mb-0 text-dark">3</p>
-                        </div>
-                        <div class="col-auto">
-                          <i class="fas fa-home fa-2x text-dark"></i>
-                        </div>
+            <div class="col-6 mb-4">
+              <a href="/my-properties.php" class="text-decoration-none">
+                <div class="card border-left-custom border-custom shadow h-100 py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <h6 class="font-weight-bold text-dark text-uppercase mb-1">Properties Listed</h6>
+                        <p class="mb-0 text-dark">3</p>
+                      </div>
+                      <div class="col-auto">
+                        <i class="fas fa-home fa-2x text-dark"></i>
                       </div>
                     </div>
                   </div>
-                </a>
-              </div>
-              <div class="col-6 mb-4">
-                <a href="/my-properties.php" class="text-decoration-none">
-                  <div class="card border-left-custom border-custom shadow h-100 py-2">
-                    <div class="card-body">
-                      <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                          <h6 class="font-weight-bold text-dark text-uppercase mb-1">Properties Sold</h6>
-                          <p class="mb-0 text-dark">1</p>
-                        </div>
-                        <div class="col-auto">
-                          <i class="fas fa-hand-holding-usd fa-2x text-dark"></i>
-                        </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-6 mb-4">
+              <a href="/my-properties.php" class="text-decoration-none">
+                <div class="card border-left-custom border-custom shadow h-100 py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <h6 class="font-weight-bold text-dark text-uppercase mb-1">Properties Sold</h6>
+                        <p class="mb-0 text-dark">1</p>
+                      </div>
+                      <div class="col-auto">
+                        <i class="fas fa-hand-holding-usd fa-2x text-dark"></i>
                       </div>
                     </div>
                   </div>
-                </a>
-              </div>
+                </div>
+              </a>
+            </div>
+            <div class="table-custom col-12">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                  <tr>
+                    <td>6</td><td>Something</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
+        </div>
         <div class="col-lg-4 col-sm-12">
           <div class="card border-custom">
             <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" alt="Card image cap">
