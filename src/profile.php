daa1d1bf9310d6b46503ea957272912e2c89784c
Andrew De Torres <andrew.de.torres@hotmail.co.uk>
2019-09-05 14:48:15 +0000

    Card changes

--- src/profile.php
+++ src/profile.php
@@ -25,7 +25,7 @@ $navIsLight = false;
     ?>
     <div class="container my-5">
       <div class="row">
-        <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="col-md-3 col-sm-12 mb-5">
           <div class="card rounded">
             <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" alt="Card image cap">
             <div class="card-body">
@@ -47,43 +47,45 @@ $navIsLight = false;
             </div>
           </div>
         </div>
-        <div class="col-lg-8 col-sm-12 mb-5">
+        <div class="col-md-9 col-sm-12 mb-5">
           <div class="row">
-            <div class="col-6 mb-4">
-              <a href="/my-properties.php" class="text-decoration-none">
-                <div class="card border-left-custom border-custom shadow h-100 py-2">
-                  <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                      <div class="col mr-2">
-                        <h6 class="font-weight-bold text-dark text-uppercase mb-1">Properties Listed</h6>
-                        <p class="mb-0 text-dark">3</p>
+              <div class="col-md-6 col-lg-6 mt-5">
+                <div class="card">
+                    <div class="card-header">
+                      <div class="custom-icon-panel card-pruple">
+                        <i class="fas fa-hand-holding-usd"></i>
                       </div>
-                      <div class="col-auto">
-                        <i class="fas fa-home fa-2x text-dark"></i>
-                      </div>
+                      <p class="card__category">Properties</p>
+                      <h3 class="card-title">
+                        3
+                        <small>Sold</small>
+                      </h3>
                     </div>
+                    <div class="card-footer"><i class="fa fa-calendar"></i>
+                      <a href="">Since Joining MOVE</a>
                   </div>
                 </div>
-              </a>
-            </div>
-            <div class="col-6 mb-4">
-              <a href="/my-properties.php" class="text-decoration-none">
-                <div class="card border-left-custom border-custom shadow h-100 py-2">
-                  <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                      <div class="col mr-2">
-                        <h6 class="font-weight-bold text-dark text-uppercase mb-1">Properties Sold</h6>
-                        <p class="mb-0 text-dark">1</p>
+              </div>
+              <div class="col-md-6 col-lg-6 mt-5">
+                <div class="card">
+                    <div class="card-header">
+                      <div class="custom-icon-panel card-pruple">
+                        <i class="fas fa-home"></i>
                       </div>
-                      <div class="col-auto">
-                        <i class="fas fa-hand-holding-usd fa-2x text-dark"></i>
-                      </div>
-                    </div>
+                      <p class="card__category">Properties</p>
+                      <p class="card-title">
+                        3
+                        <small>For Sale</small>
+                      </p>
+                  </div>
+                    <div class="card-footer"><i class="fa fa-calendar"></i>
+                      <a href="">Since Joining</a>
                   </div>
                 </div>
-              </a>
+              </div>
             </div>
-            <div class="table-custom col-12 table-striped table-responsive">
+
+            <div class="table-custom col-12 table-striped table-responsive mt-5">
               <table class="table">
                 <thead>
                   <tr>
